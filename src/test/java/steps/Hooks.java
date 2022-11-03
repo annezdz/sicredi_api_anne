83256993be2e28fff6b58834bb746a6fa7323caf
annezdz <annezdz@hotmail.com>
2022-11-03 01:32:14 -0300

    finish get and get all scenarios

--- src/test/java/steps/Hooks.java
+++ src/test/java/steps/Hooks.java
@@ -1,10 +1,7 @@
 package steps;
 
 import io.cucumber.java.Before;
-import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.builder.ResponseSpecBuilder;
-import io.restassured.filter.log.RequestLoggingFilter;
-import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
@@ -13,11 +10,9 @@ import io.restassured.specification.ResponseSpecification;
 import resources.APIResources;
 import resources.Utils;
 
-import java.beans.AppletInitializer;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 import static io.restassured.RestAssured.given;
 
@@ -28,9 +23,9 @@ public class Hooks extends Utils {
     @Before("@PutSimulation, @PutSimulationWithError,@GetByCPF,@TryGetCPFByIDWithoutSimulation, @DeleteSimulationSuccessful,@GetAllSimulations")
     public void beforeScenario() throws IOException {
 
-        newSimulation =new SimulationsSteps();
-        if(SimulationsSteps.duplicatedCPF==null) {
-            newSimulation.iCreateANewSimulationWith("11", "1000.00", "true","POST");
+        newSimulation = new SimulationsSteps();
+        if (SimulationsSteps.duplicatedCPF == null) {
+            newSimulation.iCreateANewSimulationWith("11", "1000.00", "true", "POST");
             newSimulation.callsWithVerb("postSimulationAPI", "POST");
         }
     }
@@ -47,23 +42,23 @@ public class Hooks extends Utils {
                 .get(APIResources.getAllSimulationsAPI.getResource());
 
         List<Map<String, String>> simulations = JsonPath.from(response.asString()).get();
-        while (simulations.size() > 0) {
-//            List<Integer> numbers = simulations.stream().map(c -> Integer.parseInt(c.get("id"))).collect(Collectors.toList());
-         if(simulations.size() == 0) {
-                  break;
-         }
-           List<String> ids = JsonPath.from(response.asString()).get("id");
-//           List<Integer> idsNumber = (List<Integer>) JsonPath.given(String.valueOf(response.then().extract().path("id")));
-           for(int i = 0; i < ids.size(); i++) {
-//               req = new RequestSpecBuilder().setBaseUri(getGlobalValue("baseUrl"))
-//                       .setContentType(ContentType.JSON)
-//                       .build();
-//                   int value = Integer.parseInt(ids.get(i));
-                   response = res.when()
-                           .delete(APIResources.deleteSimulationAPI.getResource() + "40");
-           }
-        }
+
+        boolean flag = true;
+        while (simulations.size() > 0 && flag == true) {
+            List<Integer> ids = JsonPath.from(response.asString()).get("id");
+            if (ids.size() > 0) {
+                for (int i = 0; i < ids.size(); i++) {
+                    response = res.when()
+                            .delete(APIResources.deleteSimulationAPI.getResource() + ids.get(0));
+                    flag = false;
+                    break;
+                }
+
+            }
+
 
         }
+
     }
+}
 
